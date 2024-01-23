fc92e5ebc01019cdef8451bd687055b526dccc2e
Zoltan.Benchea <zoltan.benchea@thedeerboy.com>
2024-01-23 16:28:43 +0200

    transfer-money-changes

--- playwright-testing/commands/transferMoney.ts
+++ playwright-testing/commands/transferMoney.ts
@@ -1,11 +1,12 @@
 import { Page } from '@playwright/test';
+import { PlaywrightUtils } from './general';
 
 export async function transferMoney(page: Page) {
   // 1. Fill "Transfer to" field
-  await page.fill('[data-testid="transfer-to"]', 'us');
+  await PlaywrightUtils.typeIntoField(page, 'transfer-to', 'us');
 
   // 2. Fill "Amount" field
-  await page.fill('[data-testid="amount"]', '1000');
+  await PlaywrightUtils.typeIntoField(page, 'amount', '1000');
 
   // 3. Click on "Begin New Mission Simulation" button
   await page.click('[data-testid="transfer-btn"]');
