28501afd8ecf7d434a006d530562bf505bdf5f71
Zoltan.Benchea <zoltan.benchea@thedeerboy.com>
2024-01-24 15:54:46 +0200

    code-refactor

--- playwright-testing/tests/expample-test.spec.ts
+++ playwright-testing/tests/expample-test.spec.ts
@@ -6,16 +6,16 @@ import { recipient, amount } from '../data/transfer';
 import { requestLoan } from '../commands/requestLoan';
 import { request } from '../data/requestLoan';
 
-test('should log in with user credentials', async ({ page }) => {
+test('should log in with user credentials then logout', async ({ page }) => {
   await loginFlow(page, url, emailAndPass);
 });
 
-test('should transfer money', async ({ page }) => {
+test('should transfer money then logout', async ({ page }) => {
   await loginFlow(page, url, emailAndPass);
   await transferMoney(page, recipient, amount);
 });
 
-test('should request a loan', async ({ page }) => {
+test('should request a loan then logout', async ({ page }) => {
   await loginFlow(page, url, emailAndPass);
   await requestLoan(page, request);
 });
